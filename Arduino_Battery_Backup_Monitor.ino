328e720855392ffda98aa2b295e2ddda99ff3565
Ryan Susman <ryansusman@gmail.com>
2024-01-08 20:35:12 -0500

    Measure Columbs

--- Arduino_Battery_Backup_Monitor.ino
+++ Arduino_Battery_Backup_Monitor.ino
@@ -4,20 +4,27 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 #include <Adafruit_ADS1X15.h>
+#include <Chrono.h> 
 
 // globals
 Adafruit_ADS1115 ads;
+Chrono myChrono;
 int LED1 = 2;
 #define analogPin A0
 int adcValue = 0;
 const char* wifiName = SECRET_SSID;
 const char* wifiPass = SECRET_PASS;
 float avg1 = 0;
+float elapsedms = 0;
 int N = 5;
 int startup_loop = 0;
 float shunt_amp = 20; // 75 mv = 20 amps
 float shunt_drop_mv = 0.075;  // 75 millivolts
-String battery_name = "Fiber_Internet";
+float battery_capasity_ah = 18;
+float max_battery_columbs = (3600 * battery_capasity_ah);
+float min_battery_columbs = (3600 * 0);
+float remaining_battery_columbs = max_battery_columbs;
+String battery_name = "Test";
 
 // setup
 void setup(void) {
@@ -84,13 +91,40 @@ double approxRollingAverage(double avg, double new_sample) {
   return avg;
 }
 
+double consumeCoulomb(float coulombs){
+  if (remaining_battery_columbs <= min_battery_columbs) {
+    Serial.println("Max Discharge");
+    return remaining_battery_columbs;
+  }
+  else{
+    remaining_battery_columbs = remaining_battery_columbs - coulombs;
+    Serial.println("Discharging...");
+    return remaining_battery_columbs;
+  }
+}
+
+double chargeCoulomb(float coulombs){
+  if (remaining_battery_columbs >= max_battery_columbs) {
+    Serial.println("Max Charge");
+    return remaining_battery_columbs;
+  }
+  else{
+    remaining_battery_columbs = remaining_battery_columbs + coulombs;
+    Serial.println("Charging...");
+    return remaining_battery_columbs;
+  }
+}
+
 // Main Loop
 void loop(void) {
   digitalWrite(LED1, HIGH);
   int16_t adc0, adc1, adc2, adc3;
   float volts0, volts1, volts2, volts3;
 
+  elapsedms = myChrono.elapsed();
   adc1 = ads.readADC_SingleEnded(1);
+  myChrono.restart();
+  
 
   volts1 = ads.computeVolts(adc1);
 
@@ -105,8 +139,23 @@ void loop(void) {
     Serial.println("Starting...");
   }
 
+  float amperage1 = (avg1 * shunt_amp) / shunt_drop_mv;
+  float coulombs = (elapsedms / 100) * amperage1;
+
+  if (amperage1 <= 0){
+    chargeCoulomb(coulombs);
+  }
+  else{
+    consumeCoulomb(coulombs);
+  }
+
+
   Serial.println("-----------------------------------------------------------");
-  Serial.print("Avg 1: ");
+  Serial.print("Coulombs Between Measurements: ");
+  Serial.print(coulombs);
+  Serial.print(" Milliseconds Between Measurements: ");
+  Serial.print(elapsedms);
+  Serial.print(" Avg 1: ");
   Serial.print(String(avg1, 7));
   Serial.print("V ");
   Serial.print("AIN1: ");
@@ -115,7 +164,7 @@ void loop(void) {
   Serial.print(String(volts1, 7));
   Serial.println("V");
 
-  float amperage1 = (avg1 * shunt_amp) / shunt_drop_mv;
+  
 
   WriteToDB(avg1, amperage1);
   digitalWrite(LED1, LOW);
