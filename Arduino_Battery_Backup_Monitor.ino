66cb4ee390cc9c96f57f39c5239d07bcb2ef4cfc
Ryan Susman <ryansusman@gmail.com>
2024-01-08 19:21:03 -0500

    Clanup

--- Arduino_Battery_Backup_Monitor.ino
+++ Arduino_Battery_Backup_Monitor.ino
@@ -1,27 +1,26 @@
+// imports
 #include <arduino_secrets.h>
-
-#include <Adafruit_ADS1X15.h>
-Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
-//Adafruit_ADS1015 ads;     /* Use this for the 12-bit version */
-int LED1 = 2;      // Assign LED1 to pin GPIO2
-/*
- * Circuits4you.com
- * Get IP Address of ESP8266 in Arduino IDE
-*/
-#define analogPin A0 /* ESP8266 Analog Pin ADC0 = A0 */
-
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
+#include <Adafruit_ADS1X15.h>
 
-int adcValue = 0;  /* Variable to store Output of ADC */
+// globals
+Adafruit_ADS1115 ads;
+int LED1 = 2;
+#define analogPin A0
+int adcValue = 0;
 const char* wifiName = SECRET_SSID;
 const char* wifiPass = SECRET_PASS;
-  
+float avg1 = 0;
+int N = 5;
+int startup_loop = 0;
+float shunt_amp = 20; // 75 mv = 20 amps
+float shunt_drop_mv = 0.075;  // 75 millivolts
+String battery_name = "Fiber_Internet";
 
- 
-void setup(void)
-{
+// setup
+void setup(void) {
   Serial.begin(115200);
 
   Serial.print("Connecting to ");
@@ -36,60 +35,45 @@ void setup(void)
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());   //You can get IP address assigned to ESP
+  Serial.println(WiFi.localIP());
   pinMode(LED1, OUTPUT);
-  // The ADC input range (or gain) can be changed via the following
-  // functions, but be careful never to exceed VDD +0.3V max, or to
-  // exceed the upper and lower limits if you adjust the input range!
-  // Setting these values incorrectly may destroy your ADC!
   //                                                                ADS1015  ADS1115
   //                                                                -------  -------
   // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
   // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
-  //ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+  // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
   // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
   // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
-  ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
-  if (!ads.begin())
-  {
+  ads.setGain(GAIN_SIXTEEN);  // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
+  if (!ads.begin()) {
     Serial.println("Failed to initialize ADS.");
-    while (1);
+    while (1)
+      ;
   }
 }
 
-float avg0 = 0;
-float avg1 = 0;
-float avg2 = 0;
-float avg3 = 0;
-int N = 5;
-
-// 75 mv = 20 amps
-float shunt_amp = 20;
-float shunt_drop_mv = 0.075; // 75 millivolts 
-String battery_name = "Fiber_Internet";
-
-int WriteToDB(float voltage, float amperage){
+// Functions
+int WriteToDB(float voltage, float amperage) {
   WiFiClientSecure client;
   HTTPClient http;
   adcValue = analogRead(analogPin);
 
-  client.setInsecure(); // Bypass SSL certificate verification
+  client.setInsecure();  // Bypass SSL certificate verification
   http.addHeader("Content-Type", "application/json");
   String serverPath = "https://us-east-1.aws.data.mongodb-api.com/app/batteryupload-ayjsz/endpoint/BatteryUpdaterV3?secret=" + String(SECRET_MONGODBSECRET);
-  String body = "{\"shunt_drop_voltage\":"+String(voltage, 7)+", \"calculated_amperage\":"+String(amperage, 7)+" , \"battery_name\":\""+battery_name+"\"}";
+  String body = "{\"shunt_drop_voltage\":" + String(voltage, 7) + ", \"calculated_amperage\":" + String(amperage, 7) + " , \"battery_name\":\"" + battery_name + "\"}";
   http.begin(client, serverPath.c_str());
   int httpResponseCode = http.POST(body);
-  if (httpResponseCode>0) {
+  if (httpResponseCode > 0) {
     Serial.print("HTTP Response code: ");
     Serial.println(httpResponseCode);
     String payload = http.getString();
     Serial.println(payload);
-  }
-  else {
+  } else {
     Serial.print("Error code: ");
     Serial.println(httpResponseCode);
   }
-    // Free resources
+  // Free resources
   http.end();
   return httpResponseCode;
 }
@@ -100,47 +84,36 @@ double approxRollingAverage(double avg, double new_sample) {
   return avg;
 }
 
-int startup_loop = 0;
-
-void loop(void)
-{
+// Main Loop
+void loop(void) {
   digitalWrite(LED1, HIGH);
   int16_t adc0, adc1, adc2, adc3;
   float volts0, volts1, volts2, volts3;
- 
-  adc0 = ads.readADC_SingleEnded(0);
+
   adc1 = ads.readADC_SingleEnded(1);
-  adc2 = ads.readADC_SingleEnded(2);
-  adc3 = ads.readADC_SingleEnded(3);
- 
-  volts0 = ads.computeVolts(adc0);
+
   volts1 = ads.computeVolts(adc1);
-  volts2 = ads.computeVolts(adc2);
-  volts3 = ads.computeVolts(adc3);
 
-  avg0 = approxRollingAverage(avg0, volts0);
   avg1 = approxRollingAverage(avg1, volts1);
-  avg2 = approxRollingAverage(avg2, volts2);
-  avg3 = approxRollingAverage(avg3, volts3);
 
-  if (startup_loop < N)
-  {
+  if (startup_loop < N) {
     startup_loop++;
     Serial.print(".");
     return;
-  }
-  else if (startup_loop == N)
-  {
+  } else if (startup_loop == N) {
     startup_loop++;
     Serial.println("Starting...");
   }
 
-
   Serial.println("-----------------------------------------------------------");
-  Serial.print("Avg 0: "); Serial.print(String(avg0, 7)); Serial.print("V "); Serial.print("AIN0: "); Serial.print(adc0); Serial.print("  "); Serial.print(String(volts0, 7)); Serial.println("V");
-  Serial.print("Avg 1: "); Serial.print(String(avg1, 7)); Serial.print("V "); Serial.print("AIN1: "); Serial.print(adc1); Serial.print("  "); Serial.print(String(volts1, 7)); Serial.println("V");
-  Serial.print("Avg 2: "); Serial.print(String(avg2, 7)); Serial.print("V "); Serial.print("AIN2: "); Serial.print(adc2); Serial.print("  "); Serial.print(String(volts2, 7)); Serial.println("V");
-  Serial.print("Avg 3: "); Serial.print(String(avg3, 7)); Serial.print("V "); Serial.print("AIN3: "); Serial.print(adc3); Serial.print("  "); Serial.print(String(volts3, 7)); Serial.println("V");
+  Serial.print("Avg 1: ");
+  Serial.print(String(avg1, 7));
+  Serial.print("V ");
+  Serial.print("AIN1: ");
+  Serial.print(adc1);
+  Serial.print("  ");
+  Serial.print(String(volts1, 7));
+  Serial.println("V");
 
   float amperage1 = (avg1 * shunt_amp) / shunt_drop_mv;
 
